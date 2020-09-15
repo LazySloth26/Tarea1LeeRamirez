c0914c78e29ae2ab381a9c8514dca54896dbf4d3
Sofia <9911sofia@gmail.com>
2020-09-15 13:10:33 -0600

    Subo los test de basic_ops
    Contiene todas las funciones con los test solicitados en pytest.

---
+++ test_operations.py
@@ -0,0 +1,71 @@
+import operations
+import pytest
+
+###### PRUEBAS PARA FUNCIÓN BASIC_OPS ######
+
+# Test para cada caso de éxito de las operaciones disponibles
+def test_suma(capsys): ##Suma
+    basic_ops = operations.operations # Llama el init de la clase operations y crea el objeto
+    basic_ops.basic_ops(basic_ops,1,2,1) # Llama la función a probar, pide el self
+    out, err = capsys.readouterr()  # Inputs y outputs de la función,los outputs salen en orden
+    assert out == "El resultado de la operación es: 3\n" # Se imprime el resultado que debe dar
+
+def test_resta(capsys): ##Resta
+    basic_ops = operations.operations
+    basic_ops.basic_ops(basic_ops,1,2,2)
+    out, err = capsys.readouterr()
+    assert out == "El resultado de la operación es: -1\n"
+
+def test_AND(capsys): ##AND
+    basic_ops = operations.operations
+    basic_ops.basic_ops(basic_ops,1,2,3)
+    out, err = capsys.readouterr()
+    assert out == "El resultado de la operación es: 0\n"
+
+def test_OR(capsys): ##OR
+    basic_ops = operations.operations  #
+    basic_ops.basic_ops(basic_ops,1,2,4)
+    out, err = capsys.readouterr()
+    assert out == "El resultado de la operación es: 3\n"
+
+
+# Test para cuando se ingresan decimales
+def test_a_decimal_V(capsys): ## Para las variables numéricas
+    basic_ops = operations.operations
+    with pytest.raises(Exception) as context: # El raise de la excepción se guarda en el context
+        basic_ops(basic_ops, 1.5, 2, 1) # Se ingresa el valor decimal en una de las variables numéricas
+        assert "ENADV" in context # Se hace un assert para verificar que el código de la excepción está en el context
+
+def test_a_decimal_OP(capsys): ## Para el valor de operation
+    basic_ops = operations.operations #
+    with pytest.raises(Exception) as context:
+        basic_ops(basic_ops, 1, 2, 1.5) # Se ingresa el valor decimal en la operación
+        assert "ENADOP" in context
+
+
+# Test para cuando se ingresan caracteres
+def test_a_character_V(capsys):  ## Para las variables numéricas
+    basic_ops = operations.operations
+    with pytest.raises(Exception) as context:
+        basic_ops(basic_ops, 1, F, 1)  # Se ingresa el caracter en una de las variables numéricas
+        assert "ENACV" in context
+
+def test_a_character_OP(capsys):
+    basic_ops = operations.operations ## Para el valor de operation
+    with pytest.raises(Exception) as context:
+        basic_ops(basic_ops, 1, 2, H)  # Se ingresa el caracter en la operación
+        assert "ENACOP" in context
+
+
+# Test para cuando se ingresan valores fuera de rango
+def test_a_range_V(capsys):  ## Para las variables numéricas
+    basic_ops = operations.operations
+    with pytest.raises(Exception) as context:
+        basic_ops(basic_ops, 150, 2, 1) # Se ingresa el valor fuera de rango en la variable numérica
+        assert "EFDRV" in context
+
+def test_a_range_OP(capsys): ## Para el valor de operation
+    basic_ops = operations.operations
+    with pytest.raises(Exception) as context:
+        basic_ops(basic_ops, 1, 2, 150) # Se ingresa el valor fuera de rango en la operación
+        assert "EFDROP" in context
