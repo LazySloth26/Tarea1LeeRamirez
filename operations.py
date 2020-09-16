f02950cea078c6e8f3c6e242747b8dcd3173a897
LazySloth26 <41813620+LazySloth26@users.noreply.github.com>
2020-09-16 15:03:06 -0600

    Revisado con flake8
    el código no presenta errores detectados por flake8

--- operations.py
+++ operations.py
@@ -1,20 +1,21 @@
 
 # Se crea una función que realiza las operaciones según el número de operación ingresado
 def basic_ops_auxiliar(x, y, operation):
-    if operation == 1: # Si se envía un 1 al parámetro operation, realiza una suma
+    if operation == 1:  # Si se envía un 1 al parámetro operation, realiza una suma
         result = x + y
         return result
-    elif operation == 2: # Si se envía un 1 al parámetro operation, realiza una resta
+    elif operation == 2:  # Si se envía un 1 al parámetro operation, realiza una resta
         result = x - y
         return result
-    elif operation == 3: # Si se envía un 1 al parámetro operation, realiza una AND
+    elif operation == 3:  # Si se envía un 1 al parámetro operation, realiza una AND
         result = x & y
         return result
-    elif operation == 4: # Si se envía un 1 al parámetro operation, realiza una OR
+    elif operation == 4:  # Si se envía un 1 al parámetro operation, realiza una OR
         result = x | y
         return result
 
-def error_handling(x, y, operation): #Esta funcion maneja los errores que se puedan producir en el array_ops
+
+def error_handling(x, operation):  # Esta funcion maneja los errores que se puedan producir en el array_ops
     '''Esta función devuelve las operaciones básicas.
     Además, tiene un código especial de errores detallados a continuación:
     ENADV = Error no se aceptan decimales en las variables matemáticas.
@@ -26,40 +27,41 @@ def error_handling(x, y, operation): #Esta funcion maneja los errores que se pue
     ELDT = Error lista de diferente tamaño.
     '''
 
-    if len(x) != len(y): #Revisa que sean del mismo tamaño
-        raise TypeError('ELDT: Por favor ingrese listas del mismo tamaño.') #levanta el error si son diferentes
-    else:
-        for i in x: #Revisa cada instancia de la lista
-            if isinstance(i, float):  # Pregunta si es float
-                raise TypeError("ENADV: Por favor ingrese solamente números enteros, no se aceptan decimales")
-            #if not isinstance(y, int):  # Pregunta si NO es un integer/entero
-                raise TypeError("ENACV: Por favor ingrese solamente números enteros, no se aceptan caracteres")
-            if i not in range(-127, 128):  # Pregunta si NO están en el rango aceptado
-                raise ValueError("EFDRV: Por favor ingrese números dentro del rango [-127,127]")
+    '''if len(x) != len(y):  # Revisa que sean del mismo tamaño
+        raise TypeError('ELDT: Por favor ingrese listas del mismo tamaño.')  # levanta el error si son diferentes
+    else:'''
+    for i in x:  # Revisa cada instancia de la lista
+        if isinstance(i, float):  # Pregunta si es float
+            raise TypeError("ENADV: Por favor ingrese solamente números enteros, no se aceptan decimales")
+        if not isinstance(i, int):  # Pregunta si NO es un integer/entero
+            raise TypeError("ENACV: Por favor ingrese solamente números enteros, no se aceptan caracteres")
+        if i not in range(-127, 128):  # Pregunta si NO están en el rango aceptado
+            raise ValueError("EFDRV: Por favor ingrese números dentro del rango [-127,127]")
 
-            # Errores para el valor de operation
-            if isinstance(operation, float):  # Pregunta si operation es un float
-                raise TypeError("ENADOP: Por favor ingrese solo valores enteros en la selección de la operación, no se aceptan decimales")
-            if not isinstance(operation, int):  # Pregunta si operation NO es un int
-                raise TypeError("ENACOP: Por favor ingrese solo valores enteros en la selección de la operación, no se aceptan letras")
-            if operation not in range(1, 5):  # Pregunta si operation está en el rango aceptado
-                raise ValueError("EFDROP: Por favor ingrese una operación en el rango aceptado [1,4]")  # Error en caso de que ingrese un número sin operación asignada
+        # Errores para el valor de operation
+        if isinstance(operation, float):  # Pregunta si operation es un float
+            raise TypeError("ENADOP: Por favor ingrese solo valores enteros en la selección de la operación, no se aceptan decimales")
+        if not isinstance(operation, int):  # Pregunta si operation NO es un int
+            raise TypeError("ENACOP: Por favor ingrese solo valores enteros en la selección de la operación, no se aceptan letras")
+        if operation not in range(1, 5):  # Pregunta si operation está en el rango aceptado
+            raise ValueError("EFDROP: Por favor ingrese una operación en el rango aceptado [1,4]")  # Error en caso de que ingrese un número sin operación asignada
 
-        for j in y:
-            if isinstance(j, float):  # Pregunta si es float
-                raise TypeError("ENADV: Por favor ingrese solamente números enteros, no se aceptan decimales")
-            if not isinstance(j, int):  # Pregunta si NO es un int
-                raise TypeError("ENACV: Por favor ingrese solamente números enteros, no se aceptan caracteres")
-            if j not in range(-127, 128):  # Pregunta si NO están en el rango aceptado
-                raise ValueError("EFDRV: Por favor ingrese números dentro del rango [-127,127]")
+    '''for j in y:
+        if isinstance(j, float):  # Pregunta si es float
+            raise TypeError("ENADV: Por favor ingrese solamente números enteros, no se aceptan decimales")
+        if not isinstance(j, int):  # Pregunta si NO es un int
+            raise TypeError("ENACV: Por favor ingrese solamente números enteros, no se aceptan caracteres")
+        if j not in range(-127, 128):  # Pregunta si NO están en el rango aceptado
+            raise ValueError("EFDRV: Por favor ingrese números dentro del rango [-127,127]")
 
-            # Errores para el valor de operation
-            if isinstance(operation, float):  # Pregunta si operation es un float
-                raise TypeError("ENADOP: Por favor ingrese solo valores enteros en la selección de la operación, no se aceptan decimales")
-            if not isinstance(operation, int):  # Pregunta si operation NO es un int
-                raise TypeError("ENACOP: Por favor ingrese solo valores enteros en la selección de la operación, no se aceptan letras")
-            if operation not in range(1, 5):  # Pregunta si operation está en el rango aceptado
-                raise ValueError("EFDROP: Por favor ingrese una operación en el rango aceptado [1,4]")# Error en caso de que ingrese un número sin operación asignada
+        # Errores para el valor de operation
+        if isinstance(operation, float):  # Pregunta si operation es un float
+            raise TypeError("ENADOP: Por favor ingrese solo valores enteros en la selección de la operación, no se aceptan decimales")
+        if not isinstance(operation, int):  # Pregunta si operation NO es un int
+            raise TypeError("ENACOP: Por favor ingrese solo valores enteros en la selección de la operación, no se aceptan letras")
+        if operation not in range(1, 5):  # Pregunta si operation está en el rango aceptado
+            raise ValueError("EFDROP: Por favor ingrese una operación en el rango aceptado [1,4]")  # Error en caso de que ingrese un número sin operación asignada
+'''
 
 
 class operations:
@@ -79,45 +81,43 @@ class operations:
 
         # El isinstance pregunta si el parámetro es o no de cierto tipo
         # Errores para las variables matemáticas
-        if isinstance(x, float) or isinstance(y, float): # Pregunta si "x" o "y" es float
+        if isinstance(x, float) or isinstance(y, float):  # Pregunta si "x" o "y" es float
             raise TypeError("ENADV. Por favor ingrese solamente números enteros, no se aceptan decimales")
-        if not isinstance(y, int):# Pregunta si "y" NO es un int
+        if not isinstance(y, int):  # Pregunta si "y" NO es un int
             raise TypeError("ENACV. Por favor ingrese solamente números enteros, no se aceptan caracteres")
-        if x not in range(-127, 128) or y not in range(-127, 128):# Pregunta si "x" o "y" NO están en el rango aceptado
+        if x not in range(-127, 128) or y not in range(-127, 128):  # Pregunta si "x" o "y" NO están en el rango aceptado
             raise ValueError("EFDRV. Por favor ingrese números dentro del rango [-127,127]")
 
         # Errores para el valor de operation
-        if isinstance(operation, float): # Pregunta si operation es un float
+        if isinstance(operation, float):  # Pregunta si operation es un float
             raise TypeError("ENADOP. Por favor ingrese solo valores enteros en la selección de la operación, no se aceptan decimales")
-        if not isinstance(operation, int): # Pregunta si operation NO es un int
+        if not isinstance(operation, int):  # Pregunta si operation NO es un int
             raise TypeError("ENACOP. Por favor ingrese solo valores enteros en la selección de la operación, no se aceptan letras")
-        if operation not in range(1, 5): # Pregunta si operation está en el rango aceptado
-            raise ValueError("EFDROP. Por favor ingrese una operación en el rango aceptado [1,4]") # Error en caso de que ingrese un número sin operación asignada
+        if operation not in range(1, 5):  # Pregunta si operation está en el rango aceptado
+            raise ValueError("EFDROP. Por favor ingrese una operación en el rango aceptado [1,4]")  # Error en caso de que ingrese un número sin operación asignada
 
+        print("El resultado de la operación es:", basic_ops_auxiliar(x, y, operation))  # Imprime el resultado de la operación solicitada
 
-        print("El resultado de la operación es:", basic_ops_auxiliar(x, y, operation)) # Imprime el resultado de la operación solicitada
+    def array_ops(self, x, y, operation):  # recibe dos arreglos y realiza operaciones con dichos arreglos
+        temp = True  # condicion para el ciclo while
+        cont = 0  # se le asigna un valor inicial al contador
+        z = []  # se inicializa un arreglo para almacenar los resultados de las operaciones
 
+        if len(x) != len(y):  # Revisa que sean del mismo tamaño
+            raise TypeError('ELDT: Por favor ingrese listas del mismo tamaño.')  # levanta el error si son diferentes
+        else:
+            error_handling(x, operation)  # llama la funcion que maneja los errores a verificar en array_ops
+            error_handling(y, operation)  # se analizan errores por separado para evitar errores de complejidad en flake8
 
-
-    def array_ops(self, x, y, operation): #recibe dos arreglos y realiza operaciones con dichos arreglos
-        temp = True #condicion para el ciclo while
-        cont = 0 #se le asigna un valor inicial al contador
-        z = [] #se inicializa un arreglo para almacenar los resultados de las operaciones
-
-        error_handling(x, y, operation) #llama la funcion que maneja los errores a verificar en array_ops
-
-        while (temp): #inicia el ciclo para pasar por cada instancia de las listas
-            aux1 = x[cont] #asigna a una variable los valores de la lista uno por uno, cambia de instancia con cada ciclo hasta que se interrumpe el while
+        while (temp):  # inicia el ciclo para pasar por cada instancia de las listas
+            aux1 = x[cont]  # asigna a una variable los valores de la lista uno por uno, cambia de instancia con cada ciclo hasta que se interrumpe el while
             aux2 = y[cont]
 
-            z.append(basic_ops_auxiliar(aux1, aux2, operation)) #agrega cada resultado de basic_ops a la lista del resultado
+            z.append(basic_ops_auxiliar(aux1, aux2, operation))  # agrega cada resultado de basic_ops a la lista del resultado
 
-            cont += 1 #aumenta el contador que genera el avance en las listas
-            if cont == len(x): #verifica que el contador no se pase del tamaño de las listas
-                temp = False #si es igual termina el ciclo
+            cont += 1  # aumenta el contador que genera el avance en las listas
+            if cont == len(x):  # verifica que el contador no se pase del tamaño de las listas
+                temp = False  # si es igual termina el ciclo
 
-        print('El resultado de la operación es: ', z) #aunque no es necesario puede que le simplifique la revisión del código
-        return z #retorna el resultado de las operaciones
-
-
-
+        print('El resultado de la operación es: ', z)  # aunque no es necesario puede que le simplifique la revisión del código
+        return z  # retorna el resultado de las operaciones
--- test_operations.py
+++ test_operations.py
@@ -1,30 +1,35 @@
 import operations
 import pytest
 
-###### PRUEBAS PARA FUNCIÓN BASIC_OPS ######
+# PRUEBAS PARA FUNCIÓN BASIC_OPS #
 
 # Test para cada caso de éxito de las operaciones disponibles
-def test_suma(capsys): ##Suma
-    basic_ops = operations.operations # Llama el init de la clase operations y crea el objeto
-    basic_ops.basic_ops(basic_ops,1,2,1) # Llama la función a probar, pide el self
-    out, err = capsys.readouterr()  # Inputs y outputs de la función,los outputs salen en orden
-    assert out == "El resultado de la operación es: 3\n" # Se imprime el resultado que debe dar
 
-def test_resta(capsys): ##Resta
+
+def test_suma(capsys):  # Suma
+    basic_ops = operations.operations  # Llama el init de la clase operations y crea el objeto
+    basic_ops.basic_ops(basic_ops, 1, 2, 1)  # Llama la función a probar, pide el self
+    out, err = capsys.readouterr()  # Inputs y outputs de la función,los outputs salen en orden
+    assert out == "El resultado de la operación es: 3\n"  # Se imprime el resultado que debe dar
+
+
+def test_resta(capsys):  # Resta
     basic_ops = operations.operations
-    basic_ops.basic_ops(basic_ops,1,2,2)
+    basic_ops.basic_ops(basic_ops, 1, 2, 2)
     out, err = capsys.readouterr()
     assert out == "El resultado de la operación es: -1\n"
 
-def test_AND(capsys): ##AND
+
+def test_AND(capsys):  # AND
     basic_ops = operations.operations
-    basic_ops.basic_ops(basic_ops,1,2,3)
+    basic_ops.basic_ops(basic_ops, 1, 2, 3)
     out, err = capsys.readouterr()
     assert out == "El resultado de la operación es: 0\n"
 
-def test_OR(capsys): ##OR
+
+def test_OR(capsys):  # OR
     basic_ops = operations.operations  #
-    basic_ops.basic_ops(basic_ops,1,2,4)
+    basic_ops.basic_ops(basic_ops, 1, 2, 4)
     out, err = capsys.readouterr()
     assert out == "El resultado de la operación es: 3\n"
 
@@ -35,84 +40,92 @@ def test_OR(capsys): ##OR
         array_ops(array_ops, [1, 2, 3], [3, 2, 1], 1) # Se ingresan las listas en una de las variables numéricas
         assert "ELDT" in context # Se hace un assert para verificar que el código de la excepción está en el context'''
 
+
 # basic ops tests
 # Test para cuando se ingresan decimales
-def test_a_decimal_V(capsys): ## Para las variables numéricas
+def test_a_decimal_V(capsys):  # Para las variables numéricas
     basic_ops = operations.operations
-    with pytest.raises(Exception) as context: # El raise de la excepción se guarda en el context
-        basic_ops(basic_ops, 1.5, 2, 1) # Se ingresa el valor decimal en una de las variables numéricas
-        assert "ENADV" in context # Se hace un assert para verificar que el código de la excepción está en el context
+    with pytest.raises(Exception) as context:  # El raise de la excepción se guarda en el context
+        basic_ops(basic_ops, 1.5, 2, 1)  # Se ingresa el valor decimal en una de las variables numéricas
+        assert "ENADV" in context  # Se hace un assert para verificar que el código de la excepción está en el context
 
-def test_a_decimal_OP(capsys): ## Para el valor de operation
-    basic_ops = operations.operations #
+
+def test_a_decimal_OP(capsys):  # Para el valor de operation
+    basic_ops = operations.operations
     with pytest.raises(Exception) as context:
-        basic_ops(basic_ops, 1, 2, 1.5) # Se ingresa el valor decimal en la operación
+        basic_ops(basic_ops, 1, 2, 1.5)  # Se ingresa el valor decimal en la operación
         assert "ENADOP" in context
 
+
 # Test para cuando se ingresan caracteres
-def test_a_character_V(capsys):  ## Para las variables numéricas
+def test_a_character_V(capsys):  # Para las variables numéricas
     basic_ops = operations.operations
     with pytest.raises(Exception) as context:
-        basic_ops(basic_ops, 1, F, 1)  # Se ingresa el caracter en una de las variables numéricas
+        basic_ops(basic_ops, 1, 'F', 1)  # Se ingresa el caracter en una de las variables numéricas
         assert "ENACV" in context
 
+
 def test_a_character_OP(capsys):
-    basic_ops = operations.operations ## Para el valor de operation
+    basic_ops = operations.operations  # Para el valor de operation
     with pytest.raises(Exception) as context:
-        basic_ops(basic_ops, 1, 2, H)  # Se ingresa el caracter en la operación
+        basic_ops(basic_ops, 1, 2, 'H')  # Se ingresa el caracter en la operación
         assert "ENACOP" in context
 
 
 # Test para cuando se ingresan valores fuera de rango
-def test_a_range_V(capsys):  ## Para las variables numéricas
+def test_a_range_V(capsys):  # Para las variables numéricas
     basic_ops = operations.operations
     with pytest.raises(Exception) as context:
-        basic_ops(basic_ops, 150, 2, 1) # Se ingresa el valor fuera de rango en la variable numérica
+        basic_ops(basic_ops, 150, 2, 1)  # Se ingresa el valor fuera de rango en la variable numérica
         assert "EFDRV" in context
 
+
 def test_a_range_OP(capsys):
-    basic_ops = operations.operations ## Para el valor de operation
+    basic_ops = operations.operations  # Para el valor de operation
     with pytest.raises(Exception) as context:
-        basic_ops(basic_ops, 1, 2, 150) # Se ingresa el valor fuera de rango en la operación
+        basic_ops(basic_ops, 1, 2, 150)  # Se ingresa el valor fuera de rango en la operación
         assert "EFDROP" in context
 
 
-
-
-#array ops tests
-def test_a_decimal_V_array(capsys): ## Para las variables numéricas
+# array ops tests
+def test_a_decimal_V_array(capsys):  # Para las variables numéricas
     array_ops = operations.operations
-    with pytest.raises(Exception) as context: # El raise de la excepción se guarda en el context
-        array_ops(array_ops, [1.5, 2, 3], [3, 2, 1], 1) # Se ingresa el valor decimal en una de las variables numéricas
-        assert "ENADV" in context # Se hace un assert para verificar que el código de la excepción está en el context
+    with pytest.raises(Exception) as context:  # El raise de la excepción se guarda en el context
+        array_ops(array_ops, [1.5, 2, 3], [3, 2, 1], 1)  # Se ingresa el valor decimal en una de las variables numéricas
+        assert "ENADV" in context  # Se hace un assert para verificar que el código de la excepción está en el context
 
-def test_a_decimal_OP_array(capsys): ## Para el valor de operation
+
+def test_a_decimal_OP_array(capsys):  # Para el valor de operation
     array_ops = operations.operations
     with pytest.raises(Exception) as context:
-        array_ops(array_ops, [1, 2, 3], [3, 2, 1], 1.5) # Se ingresa el valor decimal en la operación
+        array_ops(array_ops, [1, 2, 3], [3, 2, 1], 1.5)  # Se ingresa el valor decimal en la operación
         assert "ENADOP" in context
 
-def test_a_character_V_array(capsys):  ## Para las variables numéricas
+
+def test_a_character_V_array(capsys):  # Para las variables numéricas
     array_ops = operations.operations
     with pytest.raises(Exception) as context:
-        array_ops(array_ops, [1, 2, 3], [F, 2, 3], 1)  # Se ingresa el caracter en una de las variables numéricas
+        array_ops(array_ops, [1, 2, 3], ['F', 2, 3], 1)  # Se ingresa el caracter en una de las variables numéricas
         assert "ENACV" in context
 
+
 def test_a_character_OP_array(capsys):
-    array_ops = operations.operations ## Para el valor de operation
+    array_ops = operations.operations  # Para el valor de operation
     with pytest.raises(Exception) as context:
-        array_ops(array_ops, [1, 2, 3], [3, 2, 1], H)  # Se ingresa el caracter en la operación
+        array_ops(array_ops, [1, 2, 3], [3, 2, 1], 'H')  # Se ingresa el caracter en la operación
         assert "ENACOP" in context
 
-#Test que revisa el rango de los datos que se ingresan
-def test_a_range_V_array(capsys):  ## Para las variables numéricas
+
+# Test que revisa el rango de los datos que se ingresan
+def test_a_range_V_array(capsys):  # Para las variables numéricas
     array_ops = operations.operations
     with pytest.raises(Exception) as context:
-        array_ops(array_ops, [150, 2, 3], [3, 2, 1], 1) # Se ingresa el valor fuera de rango en la variable numérica
+        array_ops(array_ops, [150, 2, 3], [3, 2, 1], 1)  # Se ingresa el valor fuera de rango en la variable numérica
         assert "EFDRV" in context
 
+
 def test_a_range_OP_array(capsys):
-    array_ops = operations.operations ## Para el valor de operation
+    array_ops = operations.operations  # Para el valor de operation
     with pytest.raises(Exception) as context:
-        array_ops(array_ops, [1, 2, 3], [3, 2, 1], 150) # Se ingresa el valor fuera de rango en la operación
+        array_ops(array_ops, [1, 2, 3], [3, 2, 1], 150)  # Se ingresa el valor fuera de rango en la operación
         assert "EFDROP" in context
